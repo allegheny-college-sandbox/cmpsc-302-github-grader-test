f272e6e46aabffcb50398e1028969f3d2d4d8a92
Douglas Luman <dluman@allegheny.edu>
2022-03-07 12:38:05 -0500

    Update .pages.sh

--- config/.pages.sh
+++ config/.pages.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-export PAGES=$(curl -sb -H "Accept:application/json" -u dluman:$GWIZ_TOKEN $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pages)
+export PAGES_URL=$(curl -sb -H "Accept:application/json" -u dluman:$GWIZ_TOKEN $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pages)
